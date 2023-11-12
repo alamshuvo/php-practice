168462f0e4b283fdb1ecc4e635cb1040e254b45e
iftakhar <alamshuvozeny019@gmail.com>
2023-11-12 20:35:44 +0600

    php const

--- basic.php
+++ basic.php
@@ -1,10 +1,21 @@
 <?php
 // $country = "Bangladesh";
 //  echo $country;
-$age = 25 ;
+// $age = 25 ;
 
-$name = 'jhon' ;
+// $name = 'jhon' ;
 // echo "my age is $age and my name is $name";
 // echo ' My age is'. ' '  .$age .'and my name is ' . ' ' . $name;
 // echo "<a href= \"https://google.com\"> Google </a>";
 // echo '<a href= "https://google.com"> Google </a>';
+
+// $name = "jhone";
+// echo $name;
+// $name ="shuvo";
+// echo $name;
+// variable is changeable but const is not changeable
+
+const NID = 3753772239;
+$nid = NID;
+
+echo "My NID is $nid";
