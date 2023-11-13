e877e4e72e280bc9d541603840dbf1f1636411b8
iftakhar <alamshuvozeny019@gmail.com>
2023-11-13 11:20:06 +0600

    php variable type

--- basic.php
+++ basic.php
@@ -20,11 +20,51 @@
 
 // echo "My NID is $nid";
 
-$number = 12340;
-$string = "alamshuvo";
-$boolean = true;
-$floot = 10.5;
-$null = null ;
-$array= [1,2,3,4];
-// resource ;
-var_dump($objecty);
+// $number = 12340;
+// is_type(11);
+// $string = "alamshuvo";
+// $boolean = true;
+// $floot = 10.5;
+// $null = null ;
+// $array= [1,2,3,4];
+// // resource ;
+// var_dump($objecty);
+
+// $a_bool = true; 
+// echo get_debug_type($a_bool), "\n";
+
+
+// echo "hellow","world","\n";
+
+// echo "hellow","world";
+
+// $var = "bob";
+// echo $var;
+// $var ="joe";
+// echo $var;
+
+// $foo = 25;
+// $bar = &$foo ;
+// echo $bar;
+
+// $myname ;
+// $my_name;
+// $myName;
+// $MyName;
+
+
+
+// $name = "iftakhar";
+// echo "My name is {$name}";
+
+
+
+//null ,bull ,inti ,float ,object,array,resource;
+// $name ='iftakhar';
+// // $null = ['a','b','c'];
+// // echo get_debug_type($null);
+
+// if (is_string($name)) {
+//     echo $name;
+    
+// }
