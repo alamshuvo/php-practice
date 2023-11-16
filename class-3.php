4f2d5ef7ce8213c8222de4afb9ae5a8081b3acb8
iftakhar <alamshuvozeny019@gmail.com>
2023-11-16 11:00:07 +0600

    another

---
+++ class-3.php
@@ -0,0 +1,92 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if(5==6): ?>
+        <h1>Hellow world</h1>
+        <?php endif; ?>
+</body>
+</html>
+
+
+
+
+<?php
+
+// <!-- print f 
+// <!-- %s return string  -->
+// %d return 
+// $firstName = "Iftakhar";
+// $lastName = "alam";
+// printf ('my name is %s %s',$firstName ,$lastName);
+// printf ('my name is %2$s %1$s',$firstName ,$lastName);
+
+
+
+
+//sprintf:::
+// $firstName = "Iftakhar";
+// $lastName = "alam";
+//  $another = sprintf ('my name is %s %s',$firstName ,$lastName);
+//  echo $another;
+
+
+
+
+// *if condition :: 
+// *else ::
+// *else if ::
+
+
+    // **if condition ::
+
+
+    // if er condition jodi match hoi tahole if er block e dukbe na hoi if er block e dukbe na se else takhle else e cole jabe
+
+//     $a=10;
+//     $b=20;
+//     if ($a<$b) {
+//         echo "The condition is true";
+//     }
+//     else if ($a==$b){
+//         echo "This condition says that a is equal to b";
+//     }
+//     else if ($a!=$b){
+//         echo "This condition says that a is not equal to b";
+//     }
+
+//   else{
+//     echo "This condition is not true plese input valid value";
+//   }
+
+
+//real life sadaronoto amra 3 tar besi condition check na korai balo jodi 3 taar besi condition check cole ase tahole if dyea na kore switch or loop dyea korte pari amra 
+
+
+
+//jokhon amra wordpress nyea kaj korvo tokon evabei likhte hobe
+// $a=7;
+// if($a==5):
+//     echo "a equals to 5";
+//     elseif($a==6):
+//         echo "a equals to 6";
+//     else:
+//         echo "a is another";
+
+// endif;
+
+
+//wordpress e condition loop er calanor alada niom tahe ,if condition gulo sadaronoto eivabe kaj kore 
+
+//ternary operator::
+
+// $a=7;
+
+//  echo $a==7 ? 'this is 7':'this is not 7';
+
+
